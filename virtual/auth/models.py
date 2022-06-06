ff6277206b788d6a7b374160ef83ac7e66b5475c
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-06 11:39:22 +0300

    models

--- virtual/auth/models.py
+++ virtual/auth/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+# from post.models import Post
 
 # Create your models here.
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    location=models.CharField(max_length=50,blank=True,null=True)
+    url=models.CharField(max_length=100,blank=True,null=True)
+    profile_infor=models.TextField(max_length=150,null=True,blank=True)
