7e6afc1f8b635e0613d5836a0a8809e1d955c867
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-06-08 14:17:10 +0300

    admin

--- virtual/authen/admin.py
+++ virtual/authen/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Post, Profile, Comment, Like, Follow
+
+admin.site.register(Post)
+admin.site.register(Post)
+admin.site.register(Post)
+admin.site.register(Post)
+admin.site.register(Post)
 
 # Register your models here.
